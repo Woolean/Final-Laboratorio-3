5757583f14f92b95a1ec3feb65052c42d89d405a
Federico Matias Torres <matitorres10.mt@gmail.com>
2022-06-03 18:28:38 -0300

    Declaracion de Atributos y Metodos de los Usuarios y la Clase Registro

--- src/com/company/Usuarios.java
+++ src/com/company/Usuarios.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class Usuarios {
+    private String password;
+    private String user;
+
+    public Usuarios(String password, String user) {
+        this.password = password;
+        this.user = user;
+    }
 }
+
