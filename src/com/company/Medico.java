45d700de3712203a41d4e26f3ca8dc292c950244
Federico Matias Torres <matitorres10.mt@gmail.com>
2022-06-09 11:15:30 -0300

    Implementacion de los menus para los usuarios, declaracion de atributos para el sistema y algunos metodos de este

--- src/com/company/Medico.java
+++ src/com/company/Medico.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 
-public class Medico extends Usuarios implements AdministrarTareas{
+public class Medico extends Usuarios implements CrearPlandeControl, Menus {
 
     private String especializacion;
     private ArrayList<Paciente> pacientesAsignados;
@@ -53,98 +53,59 @@ public class Medico extends Usuarios implements AdministrarTareas{
         paciente.getPlanDeControl().setTiempo((int) dias);
     }
 
-    //paraadminymedicolomismo
-    public TareasDeControl agregarTareasdeContol(ArrayList<TareasDeControl> tareas) {
-
-        String descripcionNueva;
-        int opcion;
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Elegir Tratamiento");
-        for (int i=0; i<tareas.size(); i++){
-            System.out.println("Opcion "+i+" "+tareas.get(i).getDescripcion());
-        }
-        System.out.print("Opción: ");
-        opcion = scanner.nextInt();
-
-        if (tareas.get(opcion) instanceof DatoNumerico){
-            DatoNumerico dato = new DatoNumerico(tareas.get(opcion).getDescripcion(), 0);
-            return dato;
-        }
-        else if (tareas.get(opcion) instanceof DatoBoolean){
-            DatoBoolean dato = new DatoBoolean(tareas.get(opcion).getDescripcion(), false);
-            return dato;
-        }
-        else if (tareas.get(opcion) instanceof DatoTextual){
-            DatoTextual dato = new DatoTextual(tareas.get(opcion).getDescripcion(), null);
-            return dato;
-        }
-        return null;
-    }
-    //paraadminymedicolomismo
-    public void EliminarTareas(ArrayList<TareasDeControl> tareas) {
-
-        String descripcionNueva;
-        boolean seguir=false;
-        int opcion;
-        Scanner scanner = new Scanner(System.in);
-
-
-        while (!seguir) {
-            System.out.println("Elegir Tratamiento a Eliminar");
-            for (int i = 0; i < tareas.size(); i++) {
-                System.out.println("Opcion " + i + " " + tareas.get(i).getDescripcion());
-            }
-            System.out.print("Opción: ");
-            opcion = scanner.nextInt();
-            if (opcion < tareas.size()) {
-                tareas.remove(opcion);
-                seguir=true;
-            }
-        }
-
-    }
-    //agrega ademad de agregarla a un plan tambien lo puede agregar a un arraylist con las tareas por defecto
-    public TareasDeControl CrearTareaNueva() {
-
-        String descripcionNueva;
-        int opcion;
+    @Override
+    public void Menu() {
         boolean salir=false;
-        DatoNumerico datoNumerico;
-        DatoTextual datoTextual;
-        DatoBoolean datoBoolean;
+        int opcion=0;
+        Scanner scanner=new Scanner(System.in);
+        while (!salir){
 
-        Scanner scanner = new Scanner(System.in);
+            System.out.println("Opcion 1: Asignacion de Planes de Control");
+            System.out.println("Opcion 2: Control de los registros de los Pacientes");
+            System.out.println("Opcion 3: Finalizacion de Planes de Control");
+            System.out.println("Opcion 4: Salir");
 
+            System.out.printf("Ingrese una Opción: ");
+            opcion=scanner.nextInt();
 
-        while (!salir) {
-            System.out.println("Descripción de la tarea: ");
-            descripcionNueva = scanner.nextLine();
-            System.out.println("Elegir tipo de tarea: \n" +
-                    "1 - Dato numérico. \n" +
-                    "2 - Dato Textual. \n" +
-                    "3 - Dato si/no \n"
-            );
-            opcion = scanner.nextInt();
+            switch (opcion){
+                case 1:
+                    //pacientes sin plan de control
+                    //crear plan de control personalizado o uno ya echo???
+                    salir=true;
+                    break;
+                case 2:
+                    //pacientes con plan de control vigente
+                    int ano, mes, dia;
+                    for (int i = 0; i < pacientesAsignados.size(); i++) {
+                        System.out.println("Opcion " + i + " " + pacientesAsignados.get(i));
+                    }
+                    System.out.println("Ingrese una Opcion");
+                    opcion=scanner.nextInt();
+                    System.out.println("Fechas Disponibles "+pacientesAsignados.get(opcion).getPrincipiodelTratamiento()+" hasta"+ LocalDate.now().plusDays(-1));
+                    System.out.println("Ingrese Year: ");
+                    ano=scanner.nextInt();
+                    System.out.println("Ingrese Mes: ");
+                    mes=scanner.nextInt();
+                    System.out.println("Ingrese Dia: ");
+                    dia=scanner.nextInt();
 
-            switch (opcion) {
-                case 1 -> {
-                    datoNumerico = new DatoNumerico(descripcionNueva, 0);
-                    return datoNumerico;
-                }
-                case 2 -> {
-                    datoTextual = new DatoTextual(descripcionNueva, null);
-                    return datoTextual;
-                }
-                case 3 -> {
-                    datoBoolean = new DatoBoolean(descripcionNueva, false);
-                    return datoBoolean;
-                }
-                default -> {
-                    System.out.println("Opcion no Valida");
-                }
+                    System.out.println(pacientesAsignados.get(opcion).BuscarRegistro(LocalDate.of(ano, mes, dia)));
+                    break;
+                case 3:
+                    //dardealta antes de tiempo o modificar fecha
+                    for (int i = 0; i < pacientesAsignados.size(); i++) {
+                        System.out.println("Opcion " + i + " " + pacientesAsignados.get(i));
+                    }
+                    System.out.println("Ingrese una Opcion");
+                    opcion=scanner.nextInt();
+                    FinalizarPlan(pacientesAsignados.get(opcion));
+                case 4:
+                    salir=true;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
             }
         }
-        return null; //no se como pero hay que usar alguna ecepcion aca seguro
     }
 }
