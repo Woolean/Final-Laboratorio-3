1d5baf1b131f3712ff67274111cef2d11145dbaf
Federico Matias Torres <matitorres10.mt@gmail.com>
2022-06-09 15:27:30 -0300

    Agregacion de Funcionalidades a los Menus y Carga de datos de Prueba, Cambio de enum por Lista para el uso de enfermedades

--- src/com/company/Administrador.java
+++ src/com/company/Administrador.java
@@ -1,14 +1,15 @@
 package com.company;
 
-import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Administrador extends Usuarios implements CrearPlandeControl, Menus {
+public class Administrador extends Usuarios implements AdministraciondeTareasdeControl, Menus {
 
     private ArrayList<Paciente> pacientes;
     private ArrayList<Medico> medicos;
     private ArrayList<PlanDeControl> planes;
     private ArrayList<TareasDeControl> tareasbase;
+    private ArrayList<String> enfermedades;
 
     public Administrador(String password, String user, ArrayList<Paciente> pacientes, ArrayList<Medico> medicos, ArrayList<PlanDeControl> planes, ArrayList<TareasDeControl> tareasbase) {
         super(password, user);
@@ -19,17 +20,230 @@ public class Administrador extends Usuarios implements CrearPlandeControl, Menus
     }
 
     public void agregarnuevoMedico(Medico mediconuevo) {
-
+            this.medicos.add(mediconuevo);
+            //persistencia
     }
 
-    public void agregarnuevoPaciente(Paciente pacientenuevo, File pacientes, String Enfermedad, Medico medicoasignado) {
-
+    public void agregarnuevoPaciente(String Enfermedad, Medico medicoasignado, String nombre, String contrasena, String sintomas) {
+        Paciente paciente=new Paciente(contrasena, nombre, Enfermedad, sintomas);
+        this.pacientes.add(paciente);
+        //persistencia
     }
 
 
     @Override
     public void Menu() {
+        boolean salir=false;
+        int opcion=0;
+        Scanner scanner=new Scanner(System.in);
+        while (!salir){
 
+            System.out.println("Opcion 1: Ingreso de Pacientes");
+            System.out.println("Opcion 2: Ingreso de Profesionales");
+            System.out.println("Opcion 3: Administración de Tareas de Control");
+            System.out.println("Opcion 4: Administracion de Enfermedades");
+            System.out.println("Opcion 5: Salir");
+
+            System.out.printf("Ingrese una Opción: ");
+            opcion=scanner.nextInt();
+
+            switch (opcion){
+                case 1:
+                    String enfermedad;
+                    String nombre;
+                    String contrasena;
+                    String sintomas;
+                    int i=0;
+                    System.out.println("Ingrese nombre de usuario para el Paciente:");
+                    nombre=scanner.nextLine();
+                    System.out.println("Ingrese contrasena de usuario para el Paciente:");
+                    contrasena=scanner.nextLine();
+                    System.out.println("Ingrese Sintomas del Paciente:");
+                    sintomas=scanner.nextLine();
+                    System.out.println("Seleccione la enfermedad");
+                    for (String e : enfermedades){
+                        System.out.println("Opcion "+i+": "+e);
+                        i++;
+                    }
+                    System.out.println("Opcion: ");
+                    opcion=scanner.nextInt();
+                    enfermedad=enfermedades.get(opcion);
+                    for (int j = 0; j < medicos.size(); j++) {
+                        System.out.println("Opcion " + j + " " + medicos.get(i));
+                    }
+                    System.out.println("Ingrese la opcion de Medico: ");
+                    opcion=scanner.nextInt();
+
+                    agregarnuevoPaciente(enfermedad,medicos.get(opcion), nombre, contrasena, sintomas);
+                    break;
+                case 2:
+                    //persistencia faltaria
+                    String nombreMedico;
+                    String contrasenaMedico;
+                    String Especialisacion;
+                    System.out.println("Ingrese nombre de usuario para el Medico:");
+                    nombreMedico=scanner.nextLine();
+                    System.out.println("Ingrese contrasena de usuario para el Medico:");
+                    contrasenaMedico=scanner.nextLine();
+                    System.out.println("Ingrese Especialisacion del Medico:");
+                    Especialisacion=scanner.nextLine();
+                    Medico mediconuevo=new Medico(contrasenaMedico, nombreMedico, Especialisacion);
+                    agregarnuevoMedico(mediconuevo);
+                    break;
+                case 3:
+                    //dardealta antes de tiempo o modificar fecha
+                    System.out.println("Lista de enfermedades Disponibles");
+                    for (Enfermedades e : Enfermedades.values()){
+                        System.out.println(e);
+                    }
+                case 4:
+                    //administracion de tareas de control
+                    boolean salir2=false;
+                    int opcion2=0;
+                    Scanner scanner2=new Scanner(System.in);
+                    PlanDeControl Planamodificar;
+                    for (int k=0; k<planes.size(); k++){
+                        System.out.println(planes.get(k));
+                    }
+                    System.out.println("Ingrese Plan a Modificar: ");
+                    opcion2=scanner2.nextInt();
+                    Planamodificar=planes.get(opcion2);
+
+
+                    while (!salir2){
+                        System.out.println("Opcion 1: Agregar Tarea Existenete al Plan de Control");
+                        System.out.println("Opcion 2: Eliminar Tarea");
+                        System.out.println("Opcion 3: Agregar nueva Tarea");
+                        System.out.println("Opcion 4: Salir");
+
+                        System.out.printf("Ingrese una Opción: ");
+                        opcion2=scanner.nextInt();
+
+                        switch (opcion2){
+                            case 1:
+                                agregarTareasdeContol(Planamodificar.getTratamientos());
+                                //persistir cambio del plan
+                                salir=true;
+                                break;
+                            case 2:
+                                EliminarTareas(Planamodificar.getTratamientos());
+                                //persistir cambio del plan
+                                break;
+                            case 3:
+                                //crearnuevatarea para la lista así peristimos como base
+                                CrearTareaNueva(tareasbase);
+                                //persistir la lista de tareas
+                            case 4:
+                                salir=true;
+                            default:
+                                System.out.println("Opcion no valida");
+                                break;
+                        }
+                    }
+                    break;
+                case 5:
+                    salir=true;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+        }
+
+    }
+
+    //modifica la plantilla del plan de control
+    @Override
+    public void agregarTareasdeContol(ArrayList<TareasDeControl> tareas) {
+
+        String descripcionNueva;
+        int opcion;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Elegir Tratamiento");
+        for (int i=0; i<tareas.size(); i++){
+            System.out.println("Opcion "+i+" "+tareas.get(i).getDescripcion());
+        }
+        System.out.print("Opción: ");
+        opcion = scanner.nextInt();
+
+        if (tareas.get(opcion) instanceof DatoNumerico){
+            DatoNumerico dato = new DatoNumerico(tareas.get(opcion).getDescripcion(), 0);
+            tareas.add(dato);
+        }
+        else if (tareas.get(opcion) instanceof DatoBoolean){
+            DatoBoolean dato = new DatoBoolean(tareas.get(opcion).getDescripcion(), false);
+            tareas.add(dato);
+        }
+        else if (tareas.get(opcion) instanceof DatoTextual){
+            DatoTextual dato = new DatoTextual(tareas.get(opcion).getDescripcion(), null);
+            tareas.add(dato);
+        }
+    }
+    //paraadminymedicolomismo
+    @Override
+    public void EliminarTareas(ArrayList<TareasDeControl> tareas) {
+
+        boolean seguir=false;
+        int opcion;
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (!seguir) {
+            System.out.println("Elegir Tratamiento a Eliminar");
+            for (int i = 0; i < tareas.size(); i++) {
+                System.out.println("Opcion " + i + " " + tareas.get(i).getDescripcion());
+            }
+            System.out.print("Opción: ");
+            opcion = scanner.nextInt();
+            if (opcion < tareas.size()) {
+                tareas.remove(opcion);
+                seguir=true;
+            }
+        }
+    }
+    //agregar a un arraylist con las tareas por defecto y persiste
+    @Override
+    public void CrearTareaNueva(ArrayList<TareasDeControl> tareas) {
+
+        String descripcionNueva;
+        int opcion;
+        boolean salir=false;
+        DatoNumerico datoNumerico;
+        DatoTextual datoTextual;
+        DatoBoolean datoBoolean;
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (!salir) {
+            System.out.println("Descripción de la tarea: ");
+            descripcionNueva = scanner.nextLine();
+            System.out.println("Elegir tipo de tarea: \n" +
+                    "1 - Dato numérico. \n" +
+                    "2 - Dato Textual. \n" +
+                    "3 - Dato si/no \n"
+            );
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1 -> {
+                    datoNumerico = new DatoNumerico(descripcionNueva, 0);
+                    tareas.add(datoNumerico);
+                }
+                case 2 -> {
+                    datoTextual = new DatoTextual(descripcionNueva, null);
+                    tareas.add(datoTextual);
+                }
+                case 3 -> {
+                    datoBoolean = new DatoBoolean(descripcionNueva, false);
+                    tareas.add(datoBoolean);
+                }
+                default -> {
+                    System.out.println("Opcion no Valida");
+                }
+            }
+        }
+        //no se como pero hay que usar alguna ecepcion aca seguro
     }
 }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,8 +12,36 @@ public class Main {
         ArrayList<Medico> Medicos=new ArrayList<>();
         ArrayList<Administrador> Administradores=new ArrayList<>();
         ArrayList<TareasDeControl> Tareas=new ArrayList<>();
-        ArrayList<Enfermedades> Enfermedades=new ArrayList<>();
+        ArrayList<String> Enfermedades=new ArrayList<>();
         ArrayList<PlanDeControl> PlanesdeControl=new ArrayList<>();
+
+        //Carga de Datos de prueba:
+
+
+        Paciente paciente1=new Paciente("contra1", "Pablo", "BRONQUITIS", "dolor de garganta");
+        Paciente paciente2=new Paciente("contra11", "Juan", "DOLOR_GARGANTA", "dolor de garganta cuando trago");
+        Medico medico1=new Medico("contra3", "Fernando", "Oftamologo");
+        Medico medico2=new Medico("contra33", "Julio", "Pediatra");
+        DatoNumerico tarea1=new DatoNumerico("Tomar temperatura", 0);
+        DatoBoolean tarea2=new DatoBoolean("Tomar Pastilla", false);
+        String enfermedad1="BRONQUITIS";
+        String enfermedad2="DOLOR_GARGANTA";
+
+        Pacientes.add(paciente1);
+        Pacientes.add(paciente2);
+        Medicos.add(medico1);
+        Medicos.add(medico2);
+        Tareas.add(tarea1);
+        Tareas.add(tarea2);
+        Enfermedades.add(enfermedad1);
+        Enfermedades.add(enfermedad2);
+        PlanDeControl plan1=new PlanDeControl(enfermedad1, 30, null);
+        PlanDeControl plan12=new PlanDeControl(enfermedad2, 15, Tareas);
+        PlanesdeControl.add(plan1);
+        PlanesdeControl.add(plan12);
+        Administrador admin2=new Administrador("contra4", "Mateo", Pacientes, Medicos, PlanesdeControl, Tareas);
+        Administradores.add(admin2);
+        ////////////////////////////////////////
 
         Sistema hospital=new Sistema(Pacientes, Medicos, Administradores, Tareas, PlanesdeControl);
 
@@ -36,116 +64,5 @@ public class Main {
                 Seguir=true;
             }
         }
-
-        //menubase o menu en cada clase
-        /*boolean salir=false;
-        int i=0;
-        Scanner scanner=new Scanner(System.in);
-        /*while (!salir){
-
-            System.out.println("Opcion 1: Consultar Clientes");
-            System.out.println("Opcion 2: Generar nuevo Alquiler");
-            System.out.println("Opcion 3: Consultar Alquileres Vigentes");
-            System.out.println("Opcion 4: Alquileres por Devolver Hoy");
-            System.out.println("Opcion 5: Busqueda de Info de una Pelicula");
-            System.out.println("Opcion 6: Busqueda por Genero");
-            System.out.println("Opcion 7: Lo mas Alquilado");
-            System.out.println("Opcion 8: Devolver una Pelicula");
-            System.out.println("Opcion 9: Salir");
-            System.out.printf("Ingrese una Opción: ");
-            i=scanner.nextInt();
-
-            switch (i){
-                case 1:
-                    videoclubopen.ConsultarClientes();
-                    break;
-                case 2:
-                    System.out.println("\nGENERANDO ALQUILERES");
-
-                    String nombre;
-                    String peli;
-                    int opcionpeli;
-                    System.out.println("Ingrese la pelicula a Alquilar: ");
-                    for (int j=0; j<peliculas.size(); j++){
-                        System.out.println("Opcion "+j+" "+peliculas.get(j).getTitulo());
-                    }
-                    opcionpeli=scanner.nextInt();
-                    peli=peliculas.get(opcionpeli).getTitulo();
-                    System.out.println("Ingrese Nombre de Cliente: ");
-                    nombre=scanner.next();
-
-                    System.out.println(videoclubopen.Alquiler(nombre, peli));
-
-                    break;
-                case 3:
-                    System.out.println("\nALQUILERES VIGENTES");
-                    videoclubopen.ConsultarAlquileresVigentes();
-                    break;
-                case 4:
-                    System.out.println("\nALQUILERES POR DEVOLVER HOY");
-                    videoclubopen.ConsultarAlquileresDevolucion();
-                    break;
-                case 5:
-                    System.out.println("\nBUSQUEDA INFO POR TITULO");
-
-                    int opcionpeli2;
-                    String peli2;
-                    System.out.println("Ingrese la pelicula a Buscar: ");
-                    for (int j=0; j<peliculas.size(); j++){
-                        System.out.println("Opcion "+j+" "+peliculas.get(j).getTitulo());
-                    }
-                    opcionpeli2=scanner.nextInt();
-                    peli2=peliculas.get(opcionpeli2).getTitulo();
-                    System.out.println(videoclubopen.BuscarpeliculaInfo(peli2));
-                    break;
-                case 6:
-                    String generoabuscar;
-                    System.out.println("\nGENEROS: ");
-                    System.out.println(Generos.AV);
-                    System.out.println(Generos.AC);
-                    System.out.println(Generos.H);
-                    System.out.println(Generos.C);
-                    System.out.println(Generos.DO);
-                    System.out.println(Generos.DR);
-                    System.out.println("Ingrese el Genero a Buscar: ");
-                    generoabuscar=scanner.next();
-                    String[] peliculasdelgeneroelegido=videoclubopen.BuscarpeliculaGenero(generoabuscar);
-                    for (String e: peliculasdelgeneroelegido){
-                        if (e!=null){
-                            System.out.println(e);
-                        }
-                    }
-
-                    break;
-                case 7:
-                    System.out.println("LOS MAS ALQUILADOS");
-                    String[] peliculastop=videoclubopen.Ordenacionpormasalquiladas();
-                    for (String e: peliculastop){
-                        if (e!=null){
-                            System.out.println(e);
-                        }
-                    }
-
-                    break;
-                case 8:
-                    System.out.println("\nDevolucion de pelicula");
-                    int iddelafactura;
-                    videoclubopen.ConsultarAlquileresVigentes();
-                    System.out.println("Ingrese el Id de la Factura para la Devolucion de la Pelicula: ");
-                    iddelafactura=scanner.nextInt();
-                    videoclubopen.DevolverFilm(iddelafactura);
-                    break;
-                case 9:
-                    System.out.println("Saliendo......");
-                    salir=true;
-                    break;
-                default:
-                    System.out.println("Opcion invalida");
-                    break;
-            }
-        }*/
-
-
-
     }
 }
