d5311a6290718d44af54fdb5d74803bb039585ca
Woolean <julianmcoronel@hotmail.com>
2022-06-13 20:19:34 -0300

    Json funciona, falta debug algunos menús y funcionalidades

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -29,7 +29,7 @@ public class Main {
         //Creación de Archivos base
         File filePacientes = new File("files/Pacientes.json");
         File fileMedicos = new File("files/Medicos.json");
-        File fileAdmins = new File("files/Administradores.json");
+        //File fileAdmins = new File("files/Administradores.json");
         File fileTareas = new File("files/TareasDeControl.json");
         File fileEnfermedad = new File("fileS/Enfermedades.json");
         File filePlanes = new File("files/PlanesDeControl.json");
@@ -117,98 +117,13 @@ public class Main {
                 hospital.EjecutarMenu(UserenSesion);
                 Seguir=true;
             }
-            /*guardar.serializarDatos(Pacientes, filePacientes);
-            guardar.serializarDatos(Medicos, fileMedicos);
-            guardar.serializarDatos(Administradores, fileAdmins);
-            guardar.serializarDatos(Tareas, fileTareas);
-            guardar.serializarDatos(PlanesdeControl, filePlanes);
-            guardar.serializarDatos(Enfermedades, fileEnfermedad);*/
         }
 
+        //Guardar los datos de una en los archivos, por si hay que resetearlos
         /*guardar.serializarDatos(Pacientes, filePacientes);
         guardar.serializarDatos(Medicos, fileMedicos);
-        guardar.serializarDatos(Administradores, fileAdmins);
         guardar.serializarDatos(Tareas, fileTareas);
         guardar.serializarDatos(PlanesdeControl, filePlanes);
         guardar.serializarDatos(Enfermedades, fileEnfermedad);*/
-
-        //guardo los nuevos datos en los archivos - Jackson
-        /*try {
-            mapper.writeValue(filePacientes, Pacientes);
-            mapper.writeValue(fileMedicos, Medicos);
-            mapper.writeValue(fileAdmins, Administradores);
-            mapper.writeValue(fileEnfermedad, Enfermedades);
-            mapper.writeValue(filePlanes, PlanesdeControl);
-            mapper.writeValue(fileTareas, Tareas);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-        ///Guardar en Json los datos de las listas
-        //Pacientes
-        /*try {
-            FileOutputStream jsonPacientes = new FileOutputStream(filePacientes);
-            OutputStreamWriter miOutWriter = new OutputStreamWriter(jsonPacientes);
-            String json = gson.toJson(Pacientes);
-            miOutWriter.append(json);
-            miOutWriter.close();
-            jsonPacientes.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //Medicos
-        try {
-            FileOutputStream jsonMedicos = new FileOutputStream(fileMedicos);
-            OutputStreamWriter miOutWriter = new OutputStreamWriter(jsonMedicos);
-            String json = gson.toJson(Medicos);
-            miOutWriter.append(json);
-            miOutWriter.close();
-            jsonMedicos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //Administradores
-        try {
-            FileOutputStream jsonAdmins = new FileOutputStream(fileAdmins);
-            OutputStreamWriter miOutWriter = new OutputStreamWriter(jsonAdmins);
-            String json = gson.toJson(Administradores);
-            miOutWriter.append(json);
-            miOutWriter.close();
-            jsonAdmins.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //Tareas
-        try {
-            FileOutputStream jsonTareas = new FileOutputStream(fileTareas);
-            OutputStreamWriter miOutWriter = new OutputStreamWriter(jsonTareas);
-            String json = gson.toJson(Tareas);
-            miOutWriter.append(json);
-            miOutWriter.close();
-            jsonTareas.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //Enfermedades
-        try {
-            FileOutputStream jsonEnfermedad = new FileOutputStream(fileEnfermedad);
-            OutputStreamWriter miOutWriter = new OutputStreamWriter(jsonEnfermedad);
-            String json = gson.toJson(Enfermedades);
-            miOutWriter.append(json);
-            miOutWriter.close();
-            jsonEnfermedad.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //Planes
-        try {
-            FileOutputStream jsonPlanes = new FileOutputStream(filePlanes);
-            OutputStreamWriter miOutWriter = new OutputStreamWriter(jsonPlanes);
-            String json = gson.toJson(PlanesdeControl);
-            miOutWriter.append(json);
-            miOutWriter.close();
-            jsonPlanes.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
     }
 }
